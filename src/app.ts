e7e339c9caac4ab691ac8a29dc7c25f22e2ba8fa
Cezary Bober <ceski23@gmail.com>
2022-01-23 10:52:08 +0100

    feat: add express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+  const ids = Array.isArray(req.query.series) ? req.query.series : [req.query.series].filter(Boolean);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(ids);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
